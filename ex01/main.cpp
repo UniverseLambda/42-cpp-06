8992234749873049b1c1cc2144101129ffda9b2d
SAAD Clement <44324085+UniverseLambda@users.noreply.github.com>
2021-11-13 17:23:11 +0100

    Well, needed to create a Data aggregate in ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,18 +1,24 @@
 #include "serializer.hpp"
 
 #include <iostream>
+#include <iomanip>
+
+void printDataContent(std::string varName, const Data &dta) {
+	std::cout << varName << ".myBeautifulString:" << std::setw(15 - varName.size()) << " " << dta.myBeautifulString << std::endl;
+	std::cout << varName << ".someRandomNumber: " << std::setw(15 - varName.size()) << " " << dta.someRandomNumber << std::endl;
+}
 
 int main(void) {
-	Data dta = "BONCHOUUUUUUR";
+	Data dta = { "BONCHOUUUUUUR", 42 };
 	Data *ptr = &dta;
 	uintptr_t raw = serialize(ptr);
 	Data *resultPtr = deserialize(raw);
 
-	std::cout << "dta value:          " << dta << std::endl;
-	std::cout << "ptr value:          " << ptr << std::endl;
-	std::cout << "raw value:          " << raw << " (in hexadecimal: 0x" << std::hex << raw << ")" << std::endl;
-	std::cout << "resultPtr value:    " << resultPtr << std::endl;
-	std::cout << "*(resultPtr) value: " << (*resultPtr) << std::endl;
+	printDataContent("dta", dta);
+	std::cout << "ptr value:                        " << ptr << std::endl;
+	std::cout << "raw value:                        " << raw << " (in hexadecimal: 0x" << std::hex << raw << ")" << std::dec << std::endl;
+	std::cout << "resultPtr value:                  " << resultPtr << std::endl;
+	printDataContent("*(resultPtr)", (*resultPtr));
 
 	return 0;
 }
--- ex01/serializer.hpp
+++ ex01/serializer.hpp
@@ -8,8 +8,17 @@
 
 #include <string>
 
-typedef std::string Data ;
 typedef unsigned long uintptr_t;
+
+/*
+ * For the correctors, this is not a class, it's data structure (aka aggregate)
+ * as asked by the subject. So it does not have to be canonical, as this does
+ * not apply in this case.
+ */
+struct Data {
+	std::string myBeautifulString;
+	int someRandomNumber;
+};
 
 uintptr_t serialize(Data *ptr);
 Data *deserialize(uintptr_t raw);
